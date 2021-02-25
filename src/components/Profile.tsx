6e6a233d96f1293247846c65a88aa945c3914a5d
raphaelbsouza <raphaelstc@msn.com>
2021-02-25 20:52:23 -0300

    Profile atualizado

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,9 @@
+import { useContext } from "react";
+import { ChallengesContext } from "../contexts/ChallengesContext";
 import styles from "../styles/components/Profile.module.css";
 
 export function Profile() {
+    const { level } = useContext(ChallengesContext);
     return (
         <div className={styles.profileContainer}>
             <img src="https://avatars.githubusercontent.com/u/58962240?s=460&u=8444c7439f3d2fc4994475aa6833fa8b7dfd855c&v=4" alt="Raphael Souza" />
@@ -8,7 +11,7 @@ export function Profile() {
                 <strong>Raphael Souza</strong>
                 <p>
                     <img src="icons/level.svg" alt="" />
-                    Level 2</p>
+                    Level {level}</p>
             </div>
         </div>
     );
